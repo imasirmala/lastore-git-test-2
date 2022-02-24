b04f7f20097625b07114a3f2fc3b563fc7a9312d
Imas Irmala <imasirmala1903@gmail.com>
2022-02-24 17:04:07 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>git file php exercise</title>
+</head>
+<body>
+	<h1> hehooo</h1>
+</body>
+</html>
